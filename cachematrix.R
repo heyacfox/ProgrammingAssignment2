f5d7310b46b87d919544c0fccf4185ed28167da9
heyacfox <heyacfox@gmail.com>
2015-06-20 18:34:49 -0400

    Added code for Functions
    Added code for both the makeCacheMatrix and the cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This pair of functions will store a matrix
+# as an object along with its solved inverse.
 
-## Write a short comment describing this function
+# This function creates a matrix object that can
+# be used to calculate the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    # This function is used to reset the newly created
+    # matrix object's value after it has been created
+    setobjvalue <- function(y) {
+        x <<- y
+        inv <- NULL
+    }
+    # Returns the value of the object
+    get <- function() x
+    # This function assumes we've already solved
+    # for the inverse and now we are storing it
+    setinv <- function(inverse) inv <<- inverse
+    # Will return NULL until we actually solve this
+    # object
+    getinv <- function() inv
+    # "Matrix" "object" that is really a list
+    list(get = get, 
+         set = setobjvalue, 
+         getinv = getinv,
+         setinv = setinv)
 }
 
 
-## Write a short comment describing this function
+# This object solves the inverse of a matrix
+# as contained in the matrix object returned
+# by makeCacheMatrix, either through solving
+# the matrix or returning a cached solution
+# for the "object" "matrix". Note: the dot dot dot
+# is for the
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    cachedinverse <- x$getinv()
+    # If the stored inverse is not NULL, then we return
+    # the cached value
+    if(!is.null(cachedinverse)) {
+        message("Cached Matrix Inverse:")
+        return(cachedinverse)
+    }
+    # If the stored inverse is NULL, then we solve
+    # for the matrix...
+    cachedinverse <- solve(x$get(), ...)
+    # ...use the object's setinv to set its cached value...
+    x$setinv(cachedinverse)
+    # ...and return the calculated inverse matrix
+    cachedinverse
 }
